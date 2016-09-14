9cfb70af3786bd035ec7329f334116a27d830fc7
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-14 09:59:51 -0400

    Added custom subclass TableCell

---
+++ Homepwner/ItemCell.h
@@ -0,0 +1,18 @@
+//
+//  ItemCell.h
+//  Homepwner
+//
+//  Created by Dan Esrey on 2016/14/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemCell : UITableViewCell
+
+@property (nonatomic) IBOutlet UILabel *nameLabel;
+@property (nonatomic) IBOutlet UILabel *serialNumberLabel;
+@property (nonatomic) IBOutlet UILabel *valueLabel;
+
+
+@end
---
+++ Homepwner/ItemCell.m
@@ -0,0 +1,14 @@
+//
+//  ItemCell.m
+//  Homepwner
+//
+//  Created by Dan Esrey on 2016/14/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import "ItemCell.h"
+
+@implementation ItemCell
+
+
+@end
